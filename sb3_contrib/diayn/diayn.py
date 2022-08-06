b053364e2a40bc5525515acea8051eaff06e5dde
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-08-06 19:56:23 +0200

    skewfit and diayn subclass of DDPG

--- sb3_contrib/diayn/diayn.py
+++ sb3_contrib/diayn/diayn.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch as th
 import torch.nn.functional as F
 from gym import spaces
-from stable_baselines3 import SAC
+from stable_baselines3 import DDPG
 from stable_baselines3.common.buffers import ReplayBuffer
 from stable_baselines3.common.noise import ActionNoise
 from stable_baselines3.common.surgeon import Surgeon
@@ -158,30 +158,24 @@ class RecomputeReward(Surgeon):
         return replay_data
 
 
-class DIAYN(SAC):
+class DIAYN(DDPG):
     def __init__(
         self,
         env: Union[GymEnv, str],
         nb_skills: int,
         net_arch: Optional[List] = None,
-        learning_rate: Union[float, Schedule] = 0.0003,
-        buffer_size: int = 1000000,
+        learning_rate: Union[float, Schedule] = 1e-3,
+        buffer_size: int = 1_000_000,  # 1e6
         learning_starts: int = 100,
-        batch_size: int = 256,
+        batch_size: int = 100,
         tau: float = 0.005,
         gamma: float = 0.99,
-        train_freq: Union[int, Tuple[int, str]] = 1,
-        gradient_steps: int = 1,
+        train_freq: Union[int, Tuple[int, str]] = (1, "episode"),
+        gradient_steps: int = -1,
         action_noise: Optional[ActionNoise] = None,
         replay_buffer_class: Optional[ReplayBuffer] = None,
         replay_buffer_kwargs: Optional[Dict[str, Any]] = None,
         optimize_memory_usage: bool = False,
-        ent_coef: Union[str, float] = "auto",
-        target_update_interval: int = 1,
-        target_entropy: Union[str, float] = "auto",
-        use_sde: bool = False,
-        sde_sample_freq: int = -1,
-        use_sde_at_warmup: bool = False,
         tensorboard_log: Optional[str] = None,
         create_eval_env: bool = False,
         policy_kwargs: Optional[Dict[str, Any]] = None,
@@ -216,12 +210,6 @@ class DIAYN(SAC):
             replay_buffer_class,
             replay_buffer_kwargs,
             optimize_memory_usage,
-            ent_coef,
-            target_update_interval,
-            target_entropy,
-            use_sde,
-            sde_sample_freq,
-            use_sde_at_warmup,
             tensorboard_log,
             create_eval_env,
             policy_kwargs,
--- sb3_contrib/skew_fit/skew_fit.py
+++ sb3_contrib/skew_fit/skew_fit.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict, Optional, Tuple, Union
 
 import torch as th
-from stable_baselines3 import SAC
+from stable_baselines3 import DDPG
 from stable_baselines3.common.buffers import DictReplayBuffer
 from stable_baselines3.common.noise import ActionNoise
 from stable_baselines3.common.surgeon import Surgeon
@@ -10,31 +10,25 @@ from stable_baselines3.common.type_aliases import GymEnv, Schedule
 from sb3_contrib.skew_fit.goalify import Goalify
 
 
-class SkewFit(SAC):
+class SkewFit(DDPG):
     def __init__(
         self,
         env: Union[GymEnv, str],
         nb_models: int = 100,
         power: float = -1.0,
         num_presampled_goals: int = 2048,
-        learning_rate: Union[float, Schedule] = 0.0003,
+        learning_rate: Union[float, Schedule] = 0.001,
         buffer_size: int = 1000000,
         learning_starts: int = 100,
-        batch_size: int = 256,
+        batch_size: int = 100,
         tau: float = 0.005,
         gamma: float = 0.99,
-        train_freq: Union[int, Tuple[int, str]] = 1,
-        gradient_steps: int = 1,
+        train_freq: Union[int, Tuple[int, str]] = (1, "episode"),
+        gradient_steps: int = -1,
         action_noise: Optional[ActionNoise] = None,
         replay_buffer_class: Optional[DictReplayBuffer] = None,
         replay_buffer_kwargs: Optional[Dict[str, Any]] = None,
         optimize_memory_usage: bool = False,
-        ent_coef: Union[str, float] = "auto",
-        target_update_interval: int = 1,
-        target_entropy: Union[str, float] = "auto",
-        use_sde: bool = False,
-        sde_sample_freq: int = -1,
-        use_sde_at_warmup: bool = False,
         tensorboard_log: Optional[str] = None,
         create_eval_env: bool = False,
         policy_kwargs: Optional[Dict[str, Any]] = None,
@@ -68,12 +62,6 @@ class SkewFit(SAC):
             replay_buffer_class,
             replay_buffer_kwargs,
             optimize_memory_usage,
-            ent_coef,
-            target_update_interval,
-            target_entropy,
-            use_sde,
-            sde_sample_freq,
-            use_sde_at_warmup,
             tensorboard_log,
             create_eval_env,
             policy_kwargs,
